9ebe79c64b36dd1a8a83ffe8108592b34541123b
Jake Wija <jakewija@icloud.com[core]>
2020-01-06 12:59:37 -0800

    FINISH counter, create, readAll

--- datastore/index.js
+++ datastore/index.js
@@ -1,24 +1,52 @@
-const fs = require('fs');
-const path = require('path');
-const _ = require('underscore');
-const counter = require('./counter');
+const fs = require("fs");
+const path = require("path");
+const _ = require("underscore");
+const counter = require("./counter");
 
 var items = {};
 
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
-  var id = counter.getNextUniqueId();
-  items[id] = text;
-  callback(null, { id, text });
+  counter.getNextUniqueId((err, id) => {
+    fs.writeFile(exports.dataDir + "/" + id + ".txt", text, err => {
+      if (err) {
+        console.log("ERROR creating file");
+      }
+      callback(null, { id, text });
+    });
+  });
 };
 
-exports.readAll = (callback) => {
-  var data = _.map(items, (text, id) => {
-    return { id, text };
+exports.readAll = callback => {
+  fs.readdir(exports.dataDir, (err, items) => {
+    let res = [];
+    if (items.length === 0) {
+      callback(null, res);
+    } else {
+      for (let i = 0; i < items.length; i++) {
+        let filename = items[i];
+        let last = i === items.length - 1;
+        let id = filename.slice(0, 5);
+        fs.readFile(exports.dataDir + "/" + filename, (err, data) => {
+          if (err) {
+            throw err;
+          }
+          res.push({ id, text: id });
+          // res.push({ id, text: data.toString() });
+          if (last) {
+            callback(null, res);
+          }
+        });
+      }
+    }
   });
-  callback(null, data);
 };
+// return { id, text };
+// var data = _.map(items, (text, id) => {
+//   return { id, text };
+// });
+// callback(null, data);
 
 exports.readOne = (id, callback) => {
   var text = items[id];
@@ -52,7 +80,7 @@ exports.delete = (id, callback) => {
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
 
-exports.dataDir = path.join(__dirname, 'data');
+exports.dataDir = path.join(__dirname, "data");
 
 exports.initialize = () => {
   if (!fs.existsSync(exports.dataDir)) {
